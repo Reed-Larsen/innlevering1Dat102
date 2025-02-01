7af9c5d529133de7870bb8d2389916c1bf463e29
Mikal Reed-Larsen <585124@stud.hvl.com>
2025-02-01 21:46:08 +0100

    skrevet skjelett for Meny og Tekstgrensesnitt, mangler å fylle inn samt Main klassen.

--- src/no/hvl/dat102/filmarkiv/klien/Meny.java
+++ src/no/hvl/dat102/filmarkiv/klien/Meny.java
@@ -1,4 +1,18 @@
 package no.hvl.dat102.filmarkiv.klien;
 
+import no.hvl.dat102.filmarkiv.adt.FilmarkivADT;
+
 public class Meny {
+
+    private Tekstgrensesnitt tekstgr;
+    private FilmarkivADT filmarkiv;
+
+    public Meny(FilmarkivADT filmarkiv) {
+        tekstgr = new Tekstgrensesnitt();
+        this.filmarkiv = filmarkiv;
+    }
+
+    public void start(){
+        //legg inn en del forhåndsdefinerte filmer for å teste metodene
+    }
 }
--- src/no/hvl/dat102/filmarkiv/klien/Tekstgrensesnitt.java
+++ src/no/hvl/dat102/filmarkiv/klien/Tekstgrensesnitt.java
@@ -1,4 +1,33 @@
 package no.hvl.dat102.filmarkiv.klien;
 
+import no.hvl.dat102.filmarkiv.adt.FilmarkivADT;
+import no.hvl.dat102.filmarkiv.impl.Film;
+
 public class Tekstgrensesnitt {
+
+    public Film lesFilm(){
+        //TODO Skal lese inn opplysninger fra tastatur - returnere et filmobjekt
+
+        return null;
+    }
+
+    public void skrivUtFilm(Film film){
+        //TODO Skriv ut en film med alle opplysninger på skjermen, (Husk tekst for sjanger)
+    }
+
+    public void skrivUtFilmDelstrengTittel(FilmarkivADT arkiv, String delstreng){
+        //TODO Skal skrive ut alle filmer med en
+        //TODO spesiell delstreng i tittelen
+    }
+
+    public void skrivUtFilmProdusent(FilmarkivADT akriv, String delstreng){
+        //TODO Skal skrive ut alle filmer av en
+        //TODO produsent (Produsent er delstreng)
+    }
+
+    public void skrivUtStatistikk(FilmarkivADT arkiv){
+        //TODO Skal skrive ut en enkel statistikk som
+        //TODO inneholder antall filmer totalt
+        //TODO og hvor mange det er i hver sjanger
+    }
 }
