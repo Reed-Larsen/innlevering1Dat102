fbe6e5bb411f1eccb5480ebe3dcf242da1744216
585124 <mikal-44@hotmail.com>
2025-02-02 19:24:23 +0100

    Alt er ferdig

--- src/no/hvl/dat102/filmarkiv/klien/FilmarkivMain.java
+++ src/no/hvl/dat102/filmarkiv/klien/FilmarkivMain.java
@@ -2,12 +2,30 @@ package no.hvl.dat102.filmarkiv.klien;
 
 import no.hvl.dat102.filmarkiv.adt.FilmarkivADT;
 import no.hvl.dat102.filmarkiv.impl.Filmarkiv;
+import no.hvl.dat102.filmarkiv.impl.Filmarkiv2;
+
+import java.util.Scanner;
 
 public class FilmarkivMain {
     public static void main(String[] args) {
-        FilmarkivADT filma = new Filmarkiv(100);
-        Meny meny = new Meny(filma);
-        meny.start();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Velg implementasjon:");
+        System.out.println("1: Tabellimplementasjon (del 1)");
+        System.out.println("2: Lenket liste implementasjon (del 2)");
+        System.out.print("Ditt valg: ");
+        int valg = Integer.parseInt(scanner.nextLine());
+
+        FilmarkivADT filmarkiv;
+        if (valg == 1) {
+            filmarkiv = new Filmarkiv(100); // Tabellimplementasjon fra del 1
+        } else {
+            filmarkiv = new Filmarkiv2(); // Lenket liste implementasjon fra del 2
+        }
+
+        Meny meny = new Meny(filmarkiv);
+        meny.start(); // Start menyen
+    }
 
     }
-}
+
